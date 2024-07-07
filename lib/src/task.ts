6b2ca836e6e7710d8ac748fcee7a829711f1b066
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-07 16:51:31 +0200

    Implement Priority and Status enums
    * Add creationDate to be used by the React app to apply filter

--- lib/src/task.ts
+++ lib/src/task.ts
@@ -1,14 +1,25 @@
 // I'll need a TaskRequest interface to define the shape of the request body for creating a new task.
 // This will be used in the POST request to the server without id, timestamp and status.
-// If successful, the server will return the created task with the status, id and timestamp.
-// Default status will be open and not part of the request body as already specified.
+export enum Priority {
+    LOW = 'low',
+    MEDIUM = 'medium',
+    HIGH = 'high',
+}
+
+export enum Status {
+    OPEN = 'open',
+    IN_PROGRESS = 'in-progress',
+    COMPLETED = 'completed',
+}
+
 export interface Task {
     id: string;
     title: string;
     description: string;
     dueDate: number; // Unix timestamp
-    priority: 'low' | 'medium' | 'high'; // Low, Medium, High enum
-    status: 'open' | 'in-progress' | 'completed'; // Open, In_Progress, Completed enum
+    creationDate: number; // Unix timestamp
+    priority: Priority;
+    status: Status;
 }
 
 export interface Tasks {
--- task-management-server/src/data/tasks.ts
+++ task-management-server/src/data/tasks.ts
@@ -1,4 +1,6 @@
 import {
+    Priority,
+    Status,
     Task,
     Tasks,
 } from '../../../lib/src/task';
@@ -9,48 +11,54 @@ const tasks: { [key: string]: Task } = {
         title: 'Task 1',
         description: 'Description 1',
         dueDate: 1632900000000,
-        priority: 'low',
-        status: 'open'
+        creationDate: 1632900000000,
+        priority: Priority.LOW,
+        status: Status.OPEN,
     },
     2: {
         id: '2',
         title: 'Task 2',
         description: 'Description 2',
         dueDate: 1632900000000,
-        priority: 'medium',
-        status: 'in-progress'
+        creationDate: 1632900000000,
+        priority: Priority.MEDIUM,
+        status: Status.IN_PROGRESS,
     },
     3: {
         id: '3',
         title: 'Task 3',
         description: 'Description 3',
         dueDate: 1632900000000,
-        priority: 'high',
-        status: 'completed'
+        creationDate: 1632900000000,
+        priority: Priority.HIGH,
+        status: Status.COMPLETED,
     },
     4: {
         id: '4',
         title: 'Task 4',
         description: 'Description 4',
         dueDate: 1632900000000,
-        priority: 'high',
-        status: 'in-progress'
+        creationDate: 1632900000000,
+         priority: Priority.HIGH,
+        status: Status.IN_PROGRESS,
     },
     5: {
         id: '5',
         title: 'Task 5',
         description: 'Description 5',
         dueDate: 1632900000000,
-        priority: 'medium',
-        status: 'in-progress'
+        creationDate: 1632900000000,
+        priority: Priority.MEDIUM,
+        status: Status.IN_PROGRESS,
     },
     6: {
         id: '6',
         title: 'Task 6',
         description: 'Description 6',
         dueDate: 1632900000000,
-        priority: 'high',
-        status: 'completed'
+        creationDate: 1632900000000,
+        priority: Priority.HIGH,
+        status: Status.COMPLETED,
     },
 }
 
@@ -64,13 +72,14 @@ export function getTask(id: string): Task {
     return tasks[id];
 }
 
-export function addTask(taskData: Omit<Task, 'id' | 'status'>): Task {
+export function addTask(taskData: Omit<Task, 'id' | 'status' | 'creationDate'>): Task {
 
     const id = (Object.keys(tasks).length + 1).toString();
 
     const newTask: Task = {
         id: id,
-        status: 'open',
+        status: Status.OPEN,
+        creationDate: Date.now(),
         ...taskData,
     }
 
@@ -79,12 +88,13 @@ export function addTask(taskData: Omit<Task, 'id' | 'status'>): Task {
     return newTask;
 }
 
-export function updateTask(id: string, taskData: Omit<Task, 'id'>): Task | null {
+export function updateTask(id: string, taskData: Omit<Task, 'id' | 'creationDate'>): Task | null {
 
     if (tasks.hasOwnProperty(id)) {
 
         const updatedTask: Task = {
             id: id,
+            creationDate: tasks[id].creationDate,
             ...taskData,
         }
 
