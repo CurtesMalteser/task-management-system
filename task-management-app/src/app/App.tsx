01d98ad389135f6635affca1cb2cdb8a0b33e6c2
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-11 11:17:18 +0200

    Add DashboardPage.tsx

--- task-management-app/src/app/App.tsx
+++ task-management-app/src/app/App.tsx
@@ -5,10 +5,10 @@ import {
 } from 'react-router-dom';
 import ErrorPage from '../features/error/ErrorPage';
 import ROUTES from '../constants/routes';
-import WorkInProgressError from '../features/error/WorkInProgressError';
 import HomeOutlet from '../features/home/HomeOutlet';
 import TasksPage from '../features/tasks-list/TasksPage';
 import NewTask from '../features/new-task/NewTask';
+import DashboardPage from '../features/dashboard/DashboardPage';
 
 const router = createBrowserRouter([
   {
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       { path: ROUTES.HOME, element: <TasksPage /> },
-      { path: ROUTES.DASHBOARD, element: <WorkInProgressError pageTitle='Dashboard'/> },
+      { path: ROUTES.DASHBOARD, element: <DashboardPage /> },
       { path: ROUTES.NEW_TASK, element:  <NewTask />},
 
     ],
