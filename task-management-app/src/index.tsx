b90d55f73fb07500d6575554c4721db970101832
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-15 17:25:44 +0200

    Install bootstrap-sass
    * Test override bootstrap colors

--- task-management-app/src/index.tsx
+++ task-management-app/src/index.tsx
@@ -3,7 +3,6 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './app/App';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
