e7a2035e8ef9b9314a04cb7c27fce7352f5d7a8c
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-16 14:27:59 +0200

    Delete unused Counter feature

--- task-management-app/src/app/store.ts
+++ task-management-app/src/app/store.ts
@@ -1,12 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import tasksReducer from '../features/tasks-list/tasksSlice';
 import newTaskReducer from '../features/new-task/newTaskSlice';
 import taskDetailsReducer from '../features/details/taskDetailsSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     tasks: tasksReducer,
     newTask: newTaskReducer,
     taskDetails: taskDetailsReducer,
