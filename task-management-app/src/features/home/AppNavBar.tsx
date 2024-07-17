492b4dd03b77de268691c8e97569ef9ff38749f1
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-17 12:26:07 +0200

    Implement close Navbar onClick

--- task-management-app/src/features/home/AppNavBar.tsx
+++ task-management-app/src/features/home/AppNavBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,19 +7,22 @@ import ROUTES from '../../constants/routes';
 import DarkModeToggle from '../../features/dark-mode/DarkModeToggle';
 
 function AppNavBar() {
+
+    const [expanded, setExpanded] = useState(false); 
+
     return (
-        <Navbar expand="lg" className="navbar-dark bg-primary" style={{ marginBottom: "48px" }}>
+        <Navbar expand="lg" className="navbar-dark bg-primary" style={{ marginBottom: "48px" }} expanded={expanded} onToggle={setExpanded}>
             <Container>
-                <Navbar.Brand as={Link} to={ROUTES.HOME}>Employee Polls</Navbar.Brand>
+                <Navbar.Brand as={Link} to={ROUTES.HOME} onClick={() => setExpanded(false)}>Employee Polls</Navbar.Brand>
                 <div className="d-flex align-items-center">
                     <DarkModeToggle className="d-lg-none me-2"/>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(previousExpanded => !previousExpanded)} />
                 </div>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to={ROUTES.HOME}>Home</Nav.Link>
-                        <Nav.Link as={Link} to={ROUTES.DASHBOARD}>Dashboard</Nav.Link>
-                        <Nav.Link as={Link} to={ROUTES.NEW_TASK}>New</Nav.Link>
+                        <Nav.Link as={Link} to={ROUTES.HOME} onClick={() => setExpanded(false)}>Home</Nav.Link>
+                        <Nav.Link as={Link} to={ROUTES.DASHBOARD} onClick={() => setExpanded(false)}>Dashboard</Nav.Link>
+                        <Nav.Link as={Link} to={ROUTES.NEW_TASK} onClick={() => setExpanded(false)}>New</Nav.Link>
                     </Nav>
                     <DarkModeToggle className="d-none d-lg-block"/>
                 </Navbar.Collapse>
