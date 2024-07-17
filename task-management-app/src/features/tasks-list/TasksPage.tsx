0940322d214be206456ec49be468afb4434b3c85
Curtes Malteser <antonio_bastiao@hotmail.com>
2024-07-17 12:20:36 +0200

    Delete Board files, since are not used

--- task-management-app/src/features/tasks-list/TasksPage.tsx
+++ task-management-app/src/features/tasks-list/TasksPage.tsx
@@ -1,5 +1,4 @@
 import Container from "react-bootstrap/esm/Container";
-//import TasksBoard from "./components/TasksBoard";
 import TasksList from "./components/TasksList";
 
 function TasksPage() {
@@ -7,7 +6,6 @@ function TasksPage() {
   return (
     <Container className="md-9">
       <h1>Home Page</h1>
-      {/* <TasksBoard /> */}
       <TasksList />
     </Container>
   );
